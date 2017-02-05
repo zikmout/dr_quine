7c8cd0b80aac7e12c359b742f795b210ae761f82
zak <zak@debian>
2017-02-05 17:43:23 -0500

    tries on sully

---
+++ program3/Sully.c
@@ -0,0 +1,11 @@
+int i = 5;
+#include <stdio.h>
+#include <stdlib.h>
+int main(void) {
+FILE *pFile;
+char name[9] = "Sully_x.c";
+pFile = fopen(name, "w");
+	char *p="int i = %d%c; #include <stdio.h>%c#include <stdlib.h>%cint main(void) {%cFILE *pFile;%cchar name[9] = %cSully_x.c%c;%cpFile = fopen(name, %cw%c);%c%cchar *p=%c%s%c;%cfprintf(stdout, p, i, 10, 10, 10, 10, 10, 34, 34, 10, 39, 39, 10, 9, 34, p, 34, 10, 10, 10, 10);%creturn 0;%c}%c";
+fprintf(pFile, p, i, 10, 10, 10, 10, 10, 34, 34, 10, 39, 39, 10, 9, 34, p, 34, 10, 10, 10, 10);
+return 0;
+}
