f92e0f8c42292029ce3636cf61a39e732406456f
zak <zak@debian>
2017-02-05 20:47:26 -0500

    ew

--- program3/Sully.c
+++ program3/Sully.c
@@ -1,14 +1,14 @@
 int i = 5;
 #include <stdio.h>
 #include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
 int main(void) {
 i++;
-	char *p="int i = %d;%c#include <stdio.h>%c#include <stdlib.h>%cint main(void) {%ci++;%c%cchar *p=%c%s%c;%cif (i < 0) return 0;%cFILE *pFile;%cchar name[] = %cSully_x.c%c;%cname[6] = i + %c0%c;%cpFile = fopen(name, %cw%c);%cfprintf(pFile, p, i - 1, 10, 10, 10, 10, 10, 9, 34, p, 34, 10, 10, 10, 34, 34, 10, 39, 39, 10, 34, 34, 10, 10, 10, 10);%creturn 0;%c}%c";
-if (i < 0) return 0;
-FILE *pFile;
-char name[] = "Sully_x.c";
-name[6] = i + '0';
-pFile = fopen(name, "w");
-fprintf(pFile, p, i - 1, 10, 10, 10, 10, 10, 9, 34, p, 34, 10, 10, 10, 34, 34, 10, 39, 39, 10, 34, 34, 10, 10, 10, 10);
+	char *p="int i = %d;%c#include <stdio.h>%c#include <stdlib.h>%c#include <fcntl.h>%c#include <unistd.h>%cint main(void) {%ci++;%c%cchar *p=%c%s%c;%cchar path[400]; sprintf(path, %cSully_%%d.c%c, i - 1);%cint fd = open(path, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);if (fd == -1) return (1);%cdprintf(fd, p, i - 1, 10, 10, 10, 10, 10, 10, 10, 9, 34, p, 34, 10, 34, i - 1, 34, 10, 10, 10, 34, 34, 34, 34, 10, 10, 10);%csprintf(path, %cgcc -Wall -Werror -Wextra Sully_%%d.c -o Sully_%%d.c%c, i - 1, i - 1); system(path); sprintf(path, %c./Sully_%%d%c, i - 1); if (i > 1) system(path);%creturn 0;%c}%c";
+char path[400];sprintf(path,"Sully_%d.c", i - 1);
+int fd = open(path, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR); if (fd == -1) return (1);
+dprintf(fd, p, i - 1, 10, 10, 10, 10, 10, 10, 10, 9, 34, p, 34, 10, 34, i - 1, 34, 10, 10, 10, 34, 34, 34, 34, 10, 10, 10);
+sprintf(path, "gcc -Wall -Werror -Wextra Sully_%d.c -o Sully_%d.c", i - 1, i - 1);system(path);sprintf(path, "./Sully_%d", i - 1); if (i > 1) system(path);
 return 0;
 }
