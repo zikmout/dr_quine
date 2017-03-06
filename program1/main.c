f0c3c8962bcd6a0e5bc334b5fda63c06ceccde9d
Simon SICARD <ssicard@e2r8p1.42.fr>
2017-03-06 16:40:25 +0100

    qw

--- program1/main.c
+++ program1/main.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 /* first comment */
-void f() { printf("sport"); }
-char*program="#include <stdio.h>%c/* first comment */%cvoid f() { printf(%csport%c); }%cchar*program=%c%s%c;%cint main(void) {%c/* second comment */%creturn!printf(program, 10, 10, 34, 34, 10, 34, program, 34, 10, 10, 10, 10);}%c";
+int f() { return(0); }
+char*program="#include <stdio.h>%c/* first comment */%cint f() { return(0); }%cchar*program=%c%s%c;%cint main(void) {%c/* second comment */%cf();%creturn!printf(program, 10, 10, 10, 34, program, 34, 10, 10, 10, 10, 10);}%c";
 int main(void) {
 /* second comment */
-return!printf(program, 10, 10, 34, 34, 10, 34, program, 34, 10, 10, 10, 10);}
+f();
+return!printf(program, 10, 10, 10, 34, program, 34, 10, 10, 10, 10, 10);}
