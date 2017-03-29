fb17d78a4969e43ab340caee4d9f25fec8eb3163
Simon SICARD <ssicard@e2r12p14.42.fr>
2017-03-29 15:36:59 +0200

    reformating comments

--- program2/Grace.c
+++ program2/Grace.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #define decode(s,i,m,o,n,e) m##i##o##e
 #define begin decode(c,a,m,i,o,n)
-//comment
-#define F() int begin() { FILE*f = fopen("Grace_kid.c", "w");char*program="#include <stdio.h>%c#define decode(s,i,m,o,n,e) m##i##o##e%c#define begin decode(c,a,m,i,o,n)%c//comment%c#define F() int begin() { FILE*f = fopen(%cGrace_kid.c%c, %cw%c);char*program=%c%s%c;%cF() return!fprintf(f, program, 10, 10, 10, 10, 34, 34, 34, 34, 34, program, 34, 10, 10);}%c";
-F() return!fprintf(f, program, 10, 10, 10, 10, 34, 34, 34, 34, 34, program, 34, 10, 10);}
+/*
+	comment
+*/
+#define F() int begin() { FILE*f = fopen("Grace_kid.c", "w");char*program="#include <stdio.h>%c#define decode(s,i,m,o,n,e) m##i##o##e%c#define begin decode(c,a,m,i,o,n)%c/*%c%ccomment%c*/%c#define F() int begin() { FILE*f = fopen(%cGrace_kid.c%c, %cw%c);char*program=%c%s%c;%cF() return!fprintf(f, program, 10, 10, 10, 10, 9, 10, 10, 34, 34, 34, 34, 34, program, 34, 10, 10);}%c";
+F() return!fprintf(f, program, 10, 10, 10, 10, 9, 10, 10, 34, 34, 34, 34, 34, program, 34, 10, 10);}
